79892d31c01f01c2da8c785d92e363ff2355feb5
elsarcastic <125834718+elsarcastic@users.noreply.github.com>
2023-05-14 22:13:03 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,11 @@
 const botaoAlterarTema = document.getElementById("btn-alterar-tema");
-
 const body = document.querySelector("body");
-
 const imgBotaoTrocaTema = document.querySelector(".img-botao");
 
-
 botaoAlterarTema.addEventListener("click", () => {
-    body.classList.toggle("body-escuro"); 
     const modoEscuroAtivo = body.classList.contains("body-escuro");
-
+    body.classList.toggle("body-escuro");
+    
     if (modoEscuroAtivo) {
         imgBotaoTrocaTema.setAttribute("src", "./src/imagens/sun.png");
     }
